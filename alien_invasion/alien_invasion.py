7d01376a271fc4f7ed0fa60b56006a8b99836fc8
uazamula <uazamula@ukr.net>
2023-12-12 20:04:03 +0200

    Alien invasion p.253. Ship and bullets have been done

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from ship import Ship
+from bullet import Bullet
 
 from settings import Settings
 
@@ -11,16 +12,25 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height))
+        # self.screen = pygame.display.set_mode(
+        #     (self.settings.screen_width, self.settings.screen_height))
+
+        # ATTENTION! You should care for exit key if FULLSCREEN mode is used
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         while True:
             self._check_events()
             self.ship.update()
+            self._update_bullets()
+
             self._update_screen()
 
     def _check_events(self):
@@ -28,21 +38,47 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    # self.ship.rect.x+=1
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    # self.ship.rect.x+=1
-                    self.ship.moving_left = True
+                self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:# for what?
-                    self.ship.moving_right = False
-                elif event.key == pygame.K_LEFT:# for what?
-                    self.ship.moving_left = False
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            # self.ship.rect.x+=1
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            # self.ship.rect.x+=1
+            self.ship.moving_left = True
+        elif event.key in [pygame.K_q, pygame.K_ESCAPE] :
+            sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:  # for what?
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:  # for what?
+            self.ship.moving_left = False
+
+    def _fire_bullet(self):
+        if len(self.bullets)< self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        self.bullets.update()
+
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+        # print(len(self.bullets))
+
 
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
 
         pygame.display.flip()
 
--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -1,5 +1,7 @@
 import pygame
-#from alien_invasion import AlienInvasion
+
+
+# from alien_invasion import AlienInvasion
 
 class Ship:
     def __init__(self, ai_game):
@@ -9,17 +11,22 @@ class Ship:
 
         self.image = pygame.image.load('images/ship2.png')
         self.rect = self.image.get_rect()
-        self.rect.midbottom=self.screen_rect.midbottom
+        self.rect.midbottom = self.screen_rect.midbottom
 
         self.x = float(self.rect.x)
 
         self.moving_right = False
         self.moving_left = False
+
     def update(self):
-        if self.moving_right:
-            self.rect.x+=1
-        if self.moving_left:
-            self.rect.x-=1
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            # self.rect.x+=1
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left >0:
+            # self.rect.x-=1
+            self.x -= self.settings.ship_speed
+
+        self.rect.x = self.x
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
