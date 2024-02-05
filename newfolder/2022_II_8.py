aa790421da8ac402f4ae3c96ad8e75d19f5be2eb
uazamula <uazamula@ukr.net>
2024-02-05 19:15:35 +0200

    Олімпіада 2022 ІІ етап 8 задача вся, крім: ввід з файлу, вивід у файл у форматі буквачисло

--- newfolder/2022_II_8.py
+++ newfolder/2022_II_8.py
@@ -33,38 +33,38 @@
 # У випадку мату у першому рядку виведіть «Checkmate», а в наступних рядках виведіть клітинки всіх фігур, що завдають шах у довільному порядку.
 # У випадку пату у першому рядку виведіть «Stalemate».
 # В іншому випадку виведіть «Continue» та у довільному порядку всі клітинки, у які король може зробити хід.
+import copy
+
 no_king = None
+is_checkmate = False
+movement_to = []
+e = '.'
+K = 'K'
+p = 'p'
+r = 'r'
+b = 'b'
+n = 'n'
+Q = 'Q'
+chess = [[e, e, e, n, e, n, e, e, ],
+         [e, e, n, e, e, r, n, b, ],
+         [e, n, e, e, r, e, n, e, ],
+         [e, e, n, e, e, e, n, e, ],
+         [e, e, e, n, n, n, n, e, ],
+         [e, r, b, e, Q, e, e, n, ],
+         [e, e, e, e, e, e, e, e, ],
+         [e, p, e, e, e, K, e, e, ], ]
 
+chess_old = [[el for el in line] for line in chess]  # copy.deepcopy(chess)
+check = []
 
-def pos_of_king(K, chess):
-    for i in range(8):
-        for j in range(8):
-            if chess[i][j] == K:
-                return (i, j)
-    return no_king
+k_pos = None
+for i in range(8):
+    for j in range(8):
+        if chess[i][j] == K:
+            k_pos = (i, j)
 
 
 def game():
-    e = '.'
-    K = 'King'
-    p = 'pawn'
-    r = 'rook'
-    b = 'bishop'
-    n = 'knight'
-    chess = [[e, e, e, n, e, n, e, e, ],
-             [e, e, n, e, e, n, n, e, ],
-             [e, n, e, e, K, n, e, e, ],
-             [e, e, n, e, e, e, n, e, ],
-             [e, e, e, n, n, n, n, e, ],
-             [e, e, e, e, e, e, e, e, ],
-             [e, e, e, e, e, e, e, e, ],
-             [e, e, e, e, e, e, e, e, ], ]
-
-    check = []
-    k_pos = pos_of_king(K, chess)
-    if k_pos is no_king:
-        print('Where is the King?')
-        return
     for i in range(8):
         for j in range(8):
             if chess[i][j] != e:
@@ -74,7 +74,7 @@ def game():
                         print()
                         check.append([i, j])
                 # rook
-                if chess[i][j] == r:
+                if chess[i][j] == r or chess[i][j] == Q:
                     if k_pos[0] == i:
                         check.append([i, j])
                         for k in range(min(k_pos[1], j) + 1, max(k_pos[1], j)):
@@ -87,7 +87,7 @@ def game():
                             if chess[k][j] != e:
                                 check.pop()
                                 break
-                if chess[i][j] == b:
+                if chess[i][j] in [b,Q]:
                     if abs(k_pos[0] - i) == abs(k_pos[1] - j):
                         check.append([i, j])
                         for k in range(1, abs(k_pos[0] - i)):
@@ -102,10 +102,55 @@ def game():
                                 break
                 if chess[i][j] == n:
                     if abs(k_pos[0] - i) == 1 and abs(k_pos[1] - j) == 2 or \
-                       abs(k_pos[0] - i) == 2 and abs(k_pos[1] - j) == 1:
+                            abs(k_pos[0] - i) == 2 and abs(k_pos[1] - j) == 1:
                         check.append([i, j])
 
     print(check)
 
 
+def get_movement(border_condition, addx, addy):
+    if border_condition:
+        global check
+        global k_pos
+        check = []
+        k_pos = (old_k_pos[0] + addx, old_k_pos[1] + addy)
+        chess[old_k_pos[0]][old_k_pos[1]] = e
+        chess[k_pos[0]][k_pos[1]] = K
+        game()
+        for i in range(8):
+            print(chess[i])
+        print(check)
+        if not check:
+            movement_to.append([k_pos[0], k_pos[1]])
+        print(movement_to)
+        k_pos = (old_k_pos[0], old_k_pos[1])
+        for i in range(8):
+            for j in range(8):
+                chess[i][j] = chess_old[i][j]
+
+
 game()
+checkmate_maybe = copy.deepcopy(check)
+old_k_pos = (k_pos[0], k_pos[1])
+print(chess_old)
+
+get_movement(old_k_pos[0] > 0, -1, 0)
+# chess = list(chess_old)
+print(chess_old)
+get_movement(old_k_pos[1] > 0, 0, -1)
+print(checkmate_maybe)
+get_movement(old_k_pos[0] > 0 and old_k_pos[1] > 0, -1, -1)
+get_movement(old_k_pos[0] < len(chess) - 1 and old_k_pos[1] > 0, 1, -1)
+get_movement(old_k_pos[0] < len(chess) - 1, 1, 0)
+get_movement(old_k_pos[0] < len(chess) - 1 and old_k_pos[1] < len(chess[0]) - 1,
+             1, 1)
+get_movement(old_k_pos[1] < len(chess[0]) - 1, 0, 1)
+get_movement(old_k_pos[0] > 0 and old_k_pos[1] < len(chess[0]) - 1, -1, 1)
+if movement_to:
+    print('Continue')
+    print(movement_to)
+if not checkmate_maybe and not movement_to:
+    print('Stalemate')
+if checkmate_maybe and not movement_to:
+    print('Checkmate')
+    print(checkmate_maybe)
