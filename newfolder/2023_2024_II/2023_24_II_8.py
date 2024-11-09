a0b852dc0669c81749cd2c8a61d597cace54158a
uazamula <uazamula@ukr.net>
2024-11-09 21:32:16 +0200

    Олімпіада 2023-2024 ІI етап Задача H виконана на 100% максимальний час розвʼязку на тестах: 60 мс

--- newfolder/2023_2024_II/2023_24_II_8.py
+++ newfolder/2023_2024_II/2023_24_II_8.py
@@ -1,14 +1,47 @@
 n, m = map(int, input().split())
 ai = list(map(int, input().split()))
-arr = [[ai[0], 1]]
+arr = [[ai[i], 1] for i in range(n)]
 k = 0
-s = 2 ** 0
-res = ai[0]
-if n == 1:
-    while True:
-        if s >= m:
-            # print(res)
-            break
+arr.sort(key=lambda x: -x[0])
+keys = {el for el in ai}
+res = arr[0][0]
+d = {}
+for key in ai:
+    d[key] = 1 if key not in d else (d[key] + 1)
+s = 2 ** 0 if n == 1 else d[max(keys)]
+max_key = max(keys)
+
+while True:
+
+    if s >= m:
+        break
+    if n > 0:# TODO ця умова завжди виконується
+        k += 1
+
+        new_el = max_key // 2
+        if max_key % 2 == 0:
+            d[new_el] = d[max_key] if new_el not in d else (
+                        d[new_el] + d[max_key])
+            d[new_el - 1] = d[max_key] if (new_el - 1) not in d else (
+                        d[new_el - 1] + d[max_key])
+            keys.update([new_el, new_el - 1])
+        else:
+            d[new_el] = 2 * d[max_key] if new_el not in d else (
+                        d[new_el] + 2 * d[max_key])
+            keys.add(new_el)
+
+        keys.remove(max_key)
+        del d[max_key]
+        max_key = max(keys)
+        s += d[max_key]
+
+        if m <= s and max_key > 1:
+            res = max_key
+        else:
+            res = 1
+
+    else:  # TODO можна видаляти цю гілку, вона для n=1
+
         k += 1
         if len(arr) == 1:
 
@@ -40,16 +73,4 @@ if n == 1:
             res = arr[1][0]
         s += 2 ** k
 
-        if len(arr) > 1:
-            if arr[0][0] == arr[1][0]:
-                # print(arr[0][0])
-                arr[0][1] += arr[1][1]
-                arr.pop(1)
-else:
-    print('oops')
-
 print(res)
-# print('r')
-# print('k=',k,'s=',s)
-# print ('n=',n,'m=',m,ai)
-# print(arr)
